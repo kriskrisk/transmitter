dc92e4bf2ee2bfc6676b9ab4caa62ee6b3c5d0f4
kris <kristolo11@gmail.com>
2018-06-09 16:03:44 +0200

    Bug fix in reading from file

--- buffer_handler.c
+++ buffer_handler.c
@@ -19,16 +19,18 @@ void increment_buffer_idx(void) {
 
 ssize_t read_to_buffer(uint64_t *offset) {
     char *audio = malloc(psize);
+    if (audio == NULL)
+        syserr("read_to_buffer: malloc");
 
-    // Check if it was possible to read whole block
-    if (read(STDIN_FILENO, audio, psize) != psize)
+    if (fread((void *) audio, sizeof(char), psize, stdin) != psize)
         syserr("read from stdin");
 
-    audio_data *new_audio = (audio_data *)malloc(sizeof(audio_data));
+    audio_data *new_audio = (audio_data *) malloc(sizeof(audio_data));
 
     new_audio->offset = *offset;
     new_audio->audio = audio;
     cyclic_buffer[current_buffer_idx] = new_audio;
 
     *offset = *offset + psize;
+
 }
